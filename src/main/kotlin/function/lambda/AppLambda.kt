78da9432e9db7492a40e938f620abdf5705e1e7d
saifudinzezy <saifudin.zezy7@gmail.com>
2020-08-26 10:44:59 +0700

    add function lambda

--- src/main/kotlin/function/lambda/AppLambda.kt
+++ src/main/kotlin/function/lambda/AppLambda.kt
@@ -1,5 +1,13 @@
 package function.lambda
 
+/*beberapa karakteristik dari lambda berikut:
+
+1. Dalam menggunakan lambda, kita tidak perlu mendeklarasi tipe spesifik untuk nilai kembaliannya. Tipe tersebut akan ditentukan oleh kompiler secara otomatis.
+2. Walaupun merupakan sebuah fungsi, lambda tidak membutuhkan kata kunci fun dan visibility modifier saat dideklarasikan, karena lambda bersifat anonymous.
+3. Parameter yang akan ditetapkan berada di dalam kurung kurawal {}.
+4. Ketika ingin mengembalikan nilai, kata kunci return tidak diperlukan lagi karena kompiler akan secara otomatis mengembalikan nilai dari dalam body.
+5. Lambda expression dapat digunakan sebagai argumen untuk sebuah parameter dan dapat disimpan ke dalam sebuah variabel.*/
+
 fun main(){
     message()
     printMessage("udin")
