22b0881279df615d1d4c708a703d32c6fbfde854
saifudinzezy <saifudin.zezy7@gmail.com>
2020-08-27 17:10:53 +0700

    lambda argument

--- src/main/kotlin/function/library_helper/lambda_argument/AppAlso.kt
+++ src/main/kotlin/function/library_helper/lambda_argument/AppAlso.kt
@@ -1,2 +1,12 @@
 package function.library_helper.lambda_argument
 
+fun main(){
+    //Fungsi also sama seperti fungsi apply, di mana nilai yang dikembalikan adalah nilai dari konteks objek
+    //Fungsi also baiknya digunakan ketika kita ingin menggunakan konteks dari objek sebagai argumen tanpa harus mengubah nilainya.
+    val text = "Hello Kotlin"
+    val result = text.also {
+        println("value length -> ${it.length}")
+    }
+
+    println("text -> $result")
+}
